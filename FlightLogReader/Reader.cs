31d9aa318a87374d4fb841dc062aff17a0fc37b9
Florian Heider <florian.heider@siemens.com>
2022-07-20 22:31:24 +0200

    added xml doc

---
+++ FlightLogReader/Reader.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml;
+
+namespace FlightLogReader
+{
+    public class Reader
+    {
+        public void ReadFlightLog(XmlDocument flightLog)
+        {
+
+        }
+    }
+}
---
+++ FlightLogReaderTests/ReaderTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FlightLogReaderTests
+{
+    [TestClass()]
+    public class ReaderTests
+    {
+        public XmlDocument ReadXml()
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "FlightLog.xml");
+            var xml = new XmlDocument();
+            xml.LoadXml(path);
+            return xml;
+        }
+
+        [TestMethod()]
+        public void ReadFlightLogTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
