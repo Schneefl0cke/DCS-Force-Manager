60bec83887e8f608dfff7d90e580a277c5b8373a
Florian Heider <florian.heider@siemens.com>
2022-07-21 13:54:57 +0200

    sorted class

---
+++ FlightLogReader/Sorter/HasBeenDestroyedEventSorter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Facade;
+
+namespace FlightLogReader.Sorter
+{
+    public static class HasBeenDestroyedEventSorter
+    {
+        public static void Sort(List<HasBeenDestroyedEvent> hbdEvents)
+        {
+
+        }
+    }
+}
