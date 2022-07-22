607dbf5caed7ffaf37b4213c5a216e4e8c2e7b3b
Florian Heider <florian.heider@siemens.com>
2022-07-22 15:38:40 +0200

    layer

--- DCS-Force-Manager/Layer.cs
+++ DCS-Force-Manager/Layer.cs
@@ -9,11 +9,6 @@ namespace DCS_Force_Manager
 {
     internal class Layer
     {
-        public XmlDocument ReadFlightLog(string path)
-        {
-            var xml = new XmlDocument();
-            xml.LoadXml(File.ReadAllText(path));
-            return xml;
-        }
+        
     }
 }
---
+++ Layer/Layer.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml;
+
+namespace Layer
+{
+    public class Class1
+    {
+        public XmlDocument ReadFlightLog(string path)
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(File.ReadAllText(path));
+            return xml;
+        }
+    }
+}
