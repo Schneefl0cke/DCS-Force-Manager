a6514e0f457e9415150d5b24d7b8e62fcd9eda46
Florian Heider <florian.heider@siemens.com>
2022-07-22 15:40:16 +0200

    write todos

--- Layer/Layer.cs
+++ Layer/Layer.cs
@@ -2,7 +2,11 @@
 
 namespace Layer
 {
-    public class Class1
+    //TODO: 1. analyze single mission + display
+    //2. add campaign feature + analyze campaign stuff
+    //3. add squadron + squadron statistic
+    //4. add player statistics
+    public class Layer
     {
         public XmlDocument ReadFlightLog(string path)
         {
