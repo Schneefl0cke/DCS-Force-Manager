641337189b2cdfe7e9a6e3c211985c2adea07cf3
Florian Heider <florian.heider@siemens.com>
2022-07-22 15:36:03 +0200

    added winforms

---
+++ DCS-Force-Manager/Form1.cs
@@ -0,0 +1,10 @@
+namespace DCS_Force_Manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DCS-Force-Manager/Layer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace DCS_Force_Manager
+{
+    internal class Layer
+    {
+        public XmlDocument ReadFlightLog(string path)
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(File.ReadAllText(path));
+            return xml;
+        }
+    }
+}
