542c075386c84e4be161d96645324089fdcf8a0b
Florian Heider <florian.heider@siemens.com>
2022-07-26 14:12:48 +0200

    Campaign todos

---
+++ Layer/CampaignHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Layer
+{
+    public static class CampaignHandler
+    {
+        //LoadCampaign
+
+        //SaveCampaign
+
+        //Add new Campaign Mission
+    }
+}
