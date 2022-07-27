427b9cbdbab4bdfcfe9fd54ad6b6d4df500c04b3
Florian Heider <florian.heider@siemens.com>
2022-07-27 13:25:01 +0200

    source manager

--- Force Manager/Form1.cs
+++ Force Manager/Form1.cs
@@ -13,13 +13,12 @@ namespace Force_Manager
             InitializeComponent();
         }
 
+        //TODO: Load and Save playerlist when starting and exiting solution
+
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             if (MessageBox.Show("Do you really want to exit?", "Force Manager", MessageBoxButtons.YesNo) == DialogResult.No)
             {
-
-                // SET TO TRUE, SO CLOSING OF THE MAIN FORM, 
-                // SO THE APP ITSELF IS BLOCKED
                 e.Cancel = true;
             }
         }
---
+++ Layer/SourceManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Layer
+{
+    public static class SourceManager
+    {
+        public static List<string> PlayerNames = new List<string>();
+
+        public static void SavePlayerList(string path)
+        {
+            path = Path.Combine(path, ".txt");
+            var playerNames = "";
+            foreach (var player in PlayerNames)
+                playerNames += player + ';';
+
+            File.WriteAllText(path, playerNames);
+        }
+
+        //Load
+        public static void LoadPlayerFile(string path)
+        {
+            var fileContent = File.ReadAllText(path);
+            var players = fileContent.Split(';');
+            foreach (var player in players)
+                PlayerNames.Add(player);
+        }
+    }
+}
